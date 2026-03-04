0d347c596afec8de0fa0a5c3d5b63a33d8071f4e
AumJalodara <aumjalodara43@gmail.com>
2026-03-04 09:38:12 +0530

    UC12

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,30 +2,36 @@
 
 public class PalindromeCheckerApp {
      public static void main(String[] args) {
-         String input = "racecar";
 
-         PalindromeService service = new PalindromeService();
+         String input = "level";
 
-         boolean result = service.checkPalindrome(input);
+         PalindromeStrategy strategy = new StackStrategy();
+
+         boolean result = strategy.check(input);
 
          System.out.println("Input : " + input);
          System.out.println("Is Palindrome? : " + result);
      }
 }
 
-class PalindromeService {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 
-    public boolean checkPalindrome(String input) {
+class StackStrategy implements PalindromeStrategy {
 
-        int start = 0;
-        int end = input.length() - 1;
+    public boolean check(String input) {
 
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
 
         return true;
