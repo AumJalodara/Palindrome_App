7ab036a095342f500601d5962df6b39afb1fcba4
AumJalodara <aumjalodara43@gmail.com>
2026-03-04 09:45:33 +0530

    UC13

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,15 +2,21 @@
 
 public class PalindromeCheckerApp {
      public static void main(String[] args) {
-
          String input = "level";
 
          PalindromeStrategy strategy = new StackStrategy();
 
+         long startTime = System.nanoTime();
+
          boolean result = strategy.check(input);
+
+         long endTime = System.nanoTime();
+
+         long executionTime = endTime - startTime;
 
          System.out.println("Input : " + input);
          System.out.println("Is Palindrome? : " + result);
+         System.out.println("Execution Time : " + executionTime + " ns");
      }
 }
 
